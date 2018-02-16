62f49cd5a0a80d684bc46fdd8084869ac378f0ac
gavg712 <gavg712@gmail.com>
2018-02-16 17:44:04 -0500

    initial commits

--- geomuseo_app/ui.R
+++ geomuseo_app/ui.R
@@ -1,33 +1,48 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(shinythemes)
+library(markdown)
+library(ggplot2)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
-  )
-))
+navbarPage("Navbar!", theme = "css/style.css",
+           tabPanel("Plot",
+                    sidebarLayout(
+                      sidebarPanel(
+                        radioButtons("plotType", "Plot type",
+                                     c("Scatter"="p", "Line"="l")
+                        )
+                      ),
+                      mainPanel(
+                        plotOutput("plot")
+                      )
+                    )
+           ),
+           tabPanel("Summary",
+                    verbatimTextOutput("summary")
+           ),
+           navbarMenu("More",
+                      tabPanel("Table",
+                               DT::dataTableOutput("table")
+                      ),
+                      tabPanel("About",
+                               fluidRow(
+                                 column(6,
+                                        includeMarkdown("about.md")
+                                 ),
+                                 column(3,
+                                        img(class="img-polaroid",
+                                            src=paste0("http://upload.wikimedia.org/",
+                                                       "wikipedia/commons/9/92/",
+                                                       "1919_Ford_Model_T_Highboy_Coupe.jpg")),
+                                        tags$small(
+                                          "Source: Photographed at the Bay State Antique ",
+                                          "Automobile Club's July 10, 2005 show at the ",
+                                          "Endicott Estate in Dedham, MA by ",
+                                          a(href="http://commons.wikimedia.org/wiki/User:Sfoskett",
+                                            "User:Sfoskett")
+                                        )
+                                 )
+                               )
+                      )
+           )
+)
+

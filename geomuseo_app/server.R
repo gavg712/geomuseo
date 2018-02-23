cf25d11da882124dd09cca4e336a175f3679a00a
gavg712 <gavg712@gmail.com>
2018-02-23 18:08:38 -0500

    Agregar función de renderizado de mapa usando leaflet

--- geomuseo_app/server.R
+++ geomuseo_app/server.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(DT)
+library(leaflet)
 function(input, output, session) {
   output$plot <- renderPlot({
     p <- ggplot(cars, aes(speed, dist))
@@ -19,9 +20,24 @@ function(input, output, session) {
   output$table <- DT::renderDataTable({
     DT::datatable(serv_input, escape = F)
   })
-  
+
   output$map <- renderLeaflet({
-    sampling_map
+    splayer <- all_fichas
+    coordinates(splayer) <- ~x+y
+    proj4string(splayer) <- CRS("+init=epsg:32718")
+    splayer <- spTransform(splayer, CRS("+init=epsg:4326"))
+    leaflet(data = splayer) %>%
+      addProviderTiles(providers$Stamen.Terrain) %>%
+      setView(-77.81, -0.98, zoom = 10) %>%
+      addCircleMarkers(label = ~htmlEscape(codigo), popup = ~htmlEscape(nombre)) %>%
+      addMeasure(
+        position = "topleft",
+        primaryLengthUnit = "meters",
+        primaryAreaUnit = "sqmeters",
+        activeColor = "#3D535D",
+        completedColor = "#7D4479"
+      ) %>%
+      addScaleBar()
   })
 }
 

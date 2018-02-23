b5db0f88827754b246e46dc7847e4c1142ab7e4f
gavg712 <gavg712@gmail.com>
2018-02-23 18:08:05 -0500

    reducción de variables seleccionadas

--- geomuseo_app/global.R
+++ geomuseo_app/global.R
@@ -3,6 +3,8 @@ library("DBI")
 library("DT")
 library("leaflet")
 library("RColorBrewer")
+library(sp)
+library(htmltools)
 #Inputitng data and generating table for server
 dir_fichas <- list.files(path = 'www/',
                          pattern = '*.csv',
@@ -28,26 +30,21 @@ serv_input <- all_fichas %>%
   mutate(Imagen = paste0("<a href='",file.path("fotos", foto),
                                         "' target='_blank'><img src='",
                                         file.path("fotos", foto),
-                                        "' style='width:100px'> </a>")) %>%
-  select(Imagen, nombre, codigo, muestra,
-         formacion, descripcion, responsable, fecha) %>%
+                                        "' style='width:80px'> </a>")) %>%
+  select(Imagen, nombre, codigo, muestra
+         ) %>% #formacion, descripcion, responsable, fecha
   rename(Muestra = nombre,
          Imagen = Imagen,
          Código = codigo,
-         Tipo = muestra,
-         Formación = formacion,
-         Descripción = descripcion,
-         Responsable = responsable,
-         Fecha = fecha) %>%
+         Tipo = muestra#,
+         # Formación = formacion,
+         # Descripción = descripcion,
+         # Responsable = responsable,
+         # Fecha = fecha
+         ) %>%
   arrange(Muestra)
 
 #Generating the interactive map
-sampling_map <- leaflet() %>%
-  addTiles(
-    urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
-    attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
-  ) %>%
-  setView(-0.98, -77.81, zoom = 10)
 
 
 
